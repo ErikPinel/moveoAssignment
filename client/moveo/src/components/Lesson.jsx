782361702fadd88fdff6529b82624c14873c6e81
ErikPinel <erik.zusin@gmail.com>
2024-02-24 23:34:16 +0200

    before deploy

--- client/moveo/src/components/Lesson.jsx
+++ client/moveo/src/components/Lesson.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useReducer, useRef, useState } from "react";
 import LightbulbIcon from "@mui/icons-material/Lightbulb";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
@@ -11,116 +11,162 @@ import "codemirror/theme/dracula.css";
 import "codemirror/mode/javascript/javascript";
 import "codemirror/addon/edit/closetag";
 import "codemirror/addon/edit/closebrackets";
+import axios from "axios";
 
-const Lesson = ({
-  
-  title,
-  codeDescription,
-  codeSolution,
-  setCurentLesson,
-}) => {
+let isSocketIni = false;
+var initialCodeValueDb=''
+const Lesson = ({ title, codeDescription, codeSolution, setCurentLesson }) => {
   const [socket, setSocket] = useState(io.connect("http://localhost:3001"));
   const [message, setMessage] = useState("");
   const [inputValue, setInputValue] = useState("");
   const [showSolution, setShowSolution] = useState(false);
+  const [showSmiley, setShowSmiley] = useState(false);
+  const [isSmileyShown, setIsSmileyShown] = useState(false);
   const [messageReceived, setMessageReceived] = useState("");
-  const [isEditEnabled,setIsEditEnabled]=useState(false)
+  const [isEditEnabled, setIsEditEnabled] = useState(false);
 
   const editorRef = useRef(null);
+  const isSmileyShownRef = useRef(isSmileyShown);
+  const isEditEnabledRef = useRef(isEditEnabled);
   const room = title;
-
-
-
-
-
-  useEffect(() => {
-    socket.emit("join_room", room);
-   
-    async function init() {
-      editorRef.current = Codemirror.fromTextArea(
-        document.getElementById("realtimeEditor"),
-        {
-          mode: { name: "javascript", json: true },
-          theme: "dracula",
-          autoCloseTags: true,
-          autoCloseBrackets: true,
-          lineNumbers: true,
-        }
-      );
-
-      editorRef.current.on("change", (instance, changes) => {
-        const { origin } = changes;
-        const data = instance.getValue();
-        if (origin !== "setValue") {
-          socket.emit("send_message", {
-            room,
-            data,
-          });
-        }
-      });
-    }
-    init();
-    
-  }, []);
-
-
-  useEffect(() => {
-    console.log("recived main1");
-    socket.on("receive_message", (data) => {
-      console.log("recived main");
-      setMessageReceived(data.message);
-    });
-
-
-    socket.on("room_size", (data)=>{
-      console.log(data+"data size")
-      if(!isEditEnabled)
+  async function init() {
+    editorRef.current = Codemirror.fromTextArea(
+      document.getElementById("realtimeEditor"),
       {
-        if(data>1)
-        setIsEditEnabled(true)
+        mode: { name: "javascript", json: true },
+        theme: "dracula",
+        autoCloseTags: true,
+        autoCloseBrackets: true,
+        lineNumbers: true,
+        readOnly: !isEditEnabledRef.current,
         
       }
-      
-    })
+    );
+    if(initialCodeValueDb)
+    {
+      editorRef.current.setValue(initialCodeValueDb)
+      initialCodeValueDb=''
+    }
     
+    editorRef.current.on("change", (instance, changes) => {
+      const { origin } = changes;
+      const data = instance.getValue();
+      console.log("current:editor:---" + editorRef.current.getValue());
+      if (data.trim() == codeSolution.trim() && !isSmileyShownRef.current) {
+        setShowSmiley(true);
+        setIsSmileyShown(!isSmileyShown);
+        isSmileyShownRef.current = true;
+      }
 
+      if (origin !== "setValue") {
+        socket.emit("send_message", {
+          room,
+          data,
+        });
+      }
+    });
+  }
+
+  function getCurrentCodeDb(lessonName) {
+    axios
+      .get(`http://localhost:3001/api/v1/lastSavedCode/${lessonName}/`)
+      .then((response) => {
+        editorRef.current.setValue(response.data);
+      });
+  }
+
+
+  function saveCodeToDb() {
+    const lessonName=title;
+    const code =editorRef?.current?.getValue();
+    axios
+      .patch(`http://localhost:3001/api/v1/saveCode/`,{lessonName,code})
+      .then((response) => {
+        console.log(response?.msg)
+      });
+  }
+
+
+  socket.on("connect", () => {
+    socket.emit("join_room", room, socket.id);
+  });
+
+  socket.on("joined", ({ socketId }) => {
+    console.log("joinedddddd!!!!!!!!!");
+    // if (username !== location.state?.username) {
+    //     toast.success(`${username} joined the room.`);
+    //     console.log(`${username} joined`);
+    // };
+    socket.emit("sync_code", {
+      data: editorRef?.current?.getValue(),
+      socketId,
+    });
+  });
+
+  useEffect(() => {
+    socket.on("room_size", (data, id) => {
+      console.log(data + "data size");
+      if( (!editorRef?.current?.getValue())&&data==1){
+        initialCodeValueDb=getCurrentCodeDb(title)
+      }
+      if (!isEditEnabled) {
+        if (data > 1) {
+          setIsEditEnabled(true);
+          isEditEnabledRef.current = true;
+        }
+
+        console.log(isEditEnabledRef.current + "isEditEnabaled???");
+      }
+      if (id == socket.id) init();
+    });
   }, [socket]);
-
 
   useEffect(() => {
     console.log("recived");
+
     if (socket) {
       socket.on("receive_message", (data) => {
         if (data !== null) {
           editorRef.current.setValue(data);
           if (data == codeSolution) {
-            
           }
         }
       });
     }
 
     return () => {
-        socket.disconnect();
-        console.log('disconnect toom')
+      socket.disconnect();
+      console.log("disconnect toom");
     };
   }, [socket]);
 
-  const sendMessage = (e) => {
-    const data = e.target.value;
-    socket.emit("send_message", { data, room });
-  };
-
   return (
     <div className="LessonContainer">
+      {showSmiley && (
+        <div id="smileyWrapper">
+          <div
+            id="backToLessonIconWrapper"
+            onClick={() => setShowSmiley(false)}
+          >
+            <ArrowBackIcon className="icon" fontSize="large" />
+          </div>
+          <img id="smileyImg" src="../../public/smiley.jpg" />
+        </div>
+      )}
+
       <div className="codeSection">
+        <div id='saveToDbIcon' onClick={()=>saveCodeToDb()} >save to DB</div>
         <textarea id="realtimeEditor" />
       </div>
 
       <div className="descriptionSection">
         <div id="header">
-          <div onClick={() => {setCurentLesson("");}}>
-            <ArrowBackIcon />
+          <div
+            onClick={() => {
+              setCurentLesson("");
+            }}
+          >
+            <ArrowBackIcon className="icon" />
           </div>
           <h2>{title}</h2>
           <h4>{codeDescription}</h4>
@@ -133,8 +179,7 @@ const Lesson = ({
                 className="iconSolution"
                 onClick={() => setShowSolution(!showSolution)}
               >
-                {" "}
-                <VisibilityOffIcon />
+                <VisibilityOffIcon  className="icon"/>
               </div>
               <pre>{codeSolution}</pre>
             </div>
@@ -143,8 +188,7 @@ const Lesson = ({
               className="iconSolution"
               onClick={() => setShowSolution(!showSolution)}
             >
-              {" "}
-              <LightbulbIcon />
+              <LightbulbIcon className="icon" />
             </div>
           )}
         </div>
--- server/index.js
+++ server/index.js
@@ -1,9 +1,22 @@
 const express = require("express");
-const app = express();
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const helmet = require("helmet");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
 
+const Lesson = require('./model/lesson.js')
+
+/* CONFIGURATION */
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 const server = http.createServer(app);
@@ -14,44 +27,123 @@ const io = new Server(server, {
     methods: ["*"],
   },
 });
-var userCountObj={}
+
+function getAllConnectedClients(roomId) {
+  // Map
+  return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(
+    (socketId) => {
+      return {
+        socketId,
+      };
+    }
+  );
+}
+
+var userCountObj = {};
 io.on("connection", (socket) => {
-var currentRoom;
+  var currentRoom;
   console.log(`User Connected: ${socket.id}`);
 
-  socket.on("join_room",async (data) => {
-    currentRoom=data;
-    console.log('joinroom')
-    if(userCountObj[data]==null)
-    {
-        userCountObj[data]=1
-    }
-    else{
-        userCountObj[data]+=1;
+  socket.on("join_room", async (data, id) => {
+    currentRoom = data;
+    console.log("joinroom" + "id:" + id);
+    if (userCountObj[data] == null) {
+      userCountObj[data] = 1;
+    } else {
+      userCountObj[data] += 1;
     }
 
+    await socket.join(data);
 
-    
-   await socket.join(data)
-    
-    console.log("socket room number"+userCountObj[data])
-    emitRoomSize(data,userCountObj[data])
-    
+    const clients = getAllConnectedClients(data);
+    clients.forEach(({ socketId }) => {
+      io.to(socketId).emit("joined", {
+        socketId: socket.id,
+      });
+    });
+
+    console.log("socket room number" + userCountObj[data]);
+    emitRoomSize(data, userCountObj[data], id);
   });
 
   socket.on("send_message", (data) => {
-    console.log(data.data+'send_message')
-    emitCode(data)
-    
+    console.log(data.data + "send_message");
+    emitCode(data);
   });
 
-  const emitCode=(data)=>socket.to(data.room).emit("receive_message", data.data);
-  const emitRoomSize= (room,size)=>socket.to(room).emit("room_size",size)
+  socket.on("sync_code", ({ socketId, data }) => {
+    console.log("sync" + "----" + data);
+    if (data) io.to(socketId).emit("receive_message", data);
+  });
 
-  socket.on("disconnect", (data) => {console.log('disconnected: '+socket.id); userCountObj[currentRoom]-=1;})
+  const emitCode = (data) =>
+    socket.to(data.room).emit("receive_message", data.data);
+  const emitRoomSize = (room, size, id) =>
+    io.to(room).emit("room_size", size, id);
 
+  socket.on("disconnect", (data) => {
+    console.log("disconnected: " + socket.id);
+    userCountObj[currentRoom] -= 1;
+  });
 });
- 
-server.listen(3001, () => {
-  console.log("SERVER IS RUNNING");
+
+// app.post('/api/v1/createLesson/',async function(req,res){
+//     try{
+//         const { lessonName } = req.body;
+//         const newLesson = new Lesson({
+//             lessonName:lessonName,
+//             lastSavedCode:"-"
+//         });
+//         await newLesson.save();
+//         res.status(201).json(newLesson.lastSavedCode);
+//     }catch(err)
+//     {
+//         res.status(409).json({ message: err.message });
+//     }
+// });
+
+app.get('/api/v1/lastSavedCode/:lessonName/',async function(req,res,next){
+    try{
+        const { lessonName } = req.params;
+        const lesson = await Lesson.findOne({ "lessonName": lessonName });
+        res.status(200).json(lesson.lastSavedCode);
+    }catch(err){
+        res.status(409).json({ message: err.message });
+    }
 });
+
+
+
+app.patch('/api/v1/saveCode/',async function(req,res){
+    try{
+        const { lessonName,code } = req.body;
+        await Lesson.updateOne(
+            { lessonName: lessonName },
+            {
+                lessonName:lessonName,
+                lastSavedCode:code
+                
+            },
+            {
+                ignoreUndefined: true,
+            }
+        );
+        res.status(201).json("update");
+    }catch(err){
+        res.status(409).json({ message: err.message });
+    }
+});
+
+
+
+
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    autoIndex: true,
+  })
+  .then(() => {
+    server.listen(3001, () => {
+      console.log("SERVER IS RUNNING");
+    });
+  });
